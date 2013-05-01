821253c58daa3cc39750d9dae271c197dea9e5f4
David Warring <david.warring@gmail.com>
2013-05-01 16:59:16 +1000

    removed disused <eoln> rule in PDF::Grammar::Stream

--- lib/PDF/Grammar/Stream.pm
+++ lib/PDF/Grammar/Stream.pm
@@ -1,19 +1,12 @@
 use v6;
 
-# raw stream parse mode. In particular, comments are not applicable
+# raw stream parse mode. In particular, limited whitespace, no comments
 
 grammar PDF::Grammar::Stream {
     token ws {
        # Turn off white-space handling
         <!ww>
         [ "\t" ]*
-    }
-
-    token eol {"\r\n"  # ms/dos
-               | "\n"  # 'nix
-               | " \n" # 'nix - trailing blank
-               | "\r"  # mac-osx
-               | " \r" # mac-osx - trailing blank
     }
 
     regex char     { . }    # omit whitespace
