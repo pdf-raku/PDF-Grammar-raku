72faac713c292c7df8d6de10586c82557a3306e7
David Warring david.warring@gmail.com <david.warring@gmail.com>
2013-12-19 13:25:57 -0800

    added FDF specific actions class PDF::Grammar::FDF::Actions

--- lib/PDF/Grammar/FDF/Actions.pm
+++ lib/PDF/Grammar/FDF/Actions.pm
@@ -5,5 +5,5 @@ use PDF::Grammar::PDF::Actions;
 # rules for constructing PDF::Grammar::FDF AST                                                                                                 
 
 class PDF::Grammar::FDF::Actions
-    is PDF::Grammar::Actions {
+    is PDF::Grammar::PDF::Actions {
 };
